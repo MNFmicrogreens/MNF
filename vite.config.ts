7381cac9268b5da56bde820f37e2d327dee88441
MNFmicrogreens <eneznik@gmail.com>
2026-01-03 14:08:43 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
+import path from 'path'
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, '.', '');
+  const env = loadEnv(mode, '', '')
   return {
-    base: '/',          
+    base: '/',
     server: {
       port: 3000,
       host: '0.0.0.0',
@@ -16,5 +20,5 @@ export default defineConfig(({ mode }) => {
         '@': path.resolve(__dirname, '.'),
       },
     },
-  };
-});
+  }
+})
